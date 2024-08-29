48f0bfe8e633b8fe81df207cc595eaf4b0bb4213
Ehsan Gudarzi <ehsan.gdrzi@gmail.com>
2024-08-29 22:51:36 +0330

    Update Groq models

--- g4f/Provider/needs_auth/Groq.py
+++ g4f/Provider/needs_auth/Groq.py
@@ -8,8 +8,8 @@ class Groq(Openai):
     url = "https://console.groq.com/playground"
     working = True
     default_model = "mixtral-8x7b-32768"
-    models = ["mixtral-8x7b-32768", "llama2-70b-4096", "gemma-7b-it"]
-    model_aliases = {"mixtral-8x7b": "mixtral-8x7b-32768", "llama2-70b": "llama2-70b-4096"}
+    models = ["mixtral-8x7b-32768", "gemma-7b-it","distil-whisper-large-v3-en","gemma2-9b-it","gemma-7b-it","llama3-groq-70b-8192-tool-use-preview","llama3-groq-8b-8192-tool-use-preview","llama-3.1-70b-versatile","llama-3.1-8b-instant","llama-guard-3-8b","llama3-70b-8192","llama3-8b-8192","whisper-large-v3"]
+    model_aliases = {"mixtral-8x7b": "mixtral-8x7b-32768"}
 
     @classmethod
     def create_async_generator(
