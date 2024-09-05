632382a3145819058475f11cb234401e9ba0dfe8
Ehsan Gudarzi <ehsan.gdrzi@gmail.com>
2024-09-05 23:04:23 +0330

    Fix test host

--- test_api.py
+++ test_api.py
@@ -1,2 +1,2 @@
 from g4f.api import run_api
-run_api(port=11338)
+run_api(host='127.0.0.1', port=11338)
--- test_gui.py
+++ test_gui.py
@@ -1,2 +1,2 @@
 from g4f.gui import run_gui
-run_gui(port=18080)
+run_gui(host='127.0.0.1', port=18080)
